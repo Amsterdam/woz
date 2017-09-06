ff9067eba9d7cce3df8861f37accb1059207f4ee
Marten Hoekstra <marten@dolhuis.nl>
2017-09-06 15:07:25 +0200

    tg-1222 andere kadastrale identeficatie

--- web/app/woz/api/views.py
+++ web/app/woz/api/views.py
@@ -14,13 +14,24 @@ log = logging.getLogger(__name__)
 
 
 class WaardeView(views.APIView):
+    """
+        kadastraal_object: ASD15 S 09256 A 0002
+
+        retourneert de WOZ waarde in RESTRICTED YEARS indien kadastraal object bestaat in WOZ data
+        en één van de gebruiksdoelen van het verblijfsobject bij de nummeraanduiding van het WOZ
+        object een woning is. Als waarde geldt die, die het meest recent (naar
+        begindatum_beschikking_object) is vastgesteld
+     """
+
     def get(self, request, *args, **kwargs):
         kadastraal_object = request.query_params['kadastraal_object']
         if kadastraal_object is None:
             return Response([])
+        kadastrale_identificatie = str.split(kadastraal_object)
 
-        woz_objecten = self._get_woz_woningen_from(kadastraal_object)
         response = []
+
+        woz_objecten = self._get_woz_woningen_from(kadastrale_identificatie)
         for woz_object in woz_objecten:
             waardebeschikkingen = models.WOZWaardeBeschikking.objects.filter(
                 woz_object=woz_object.woz_objectnummer
@@ -33,15 +44,20 @@ class WaardeView(views.APIView):
                 'woz_object': woz_object.woz_objectnummer,
                 'waarden': output_waarden
             })
+
         return Response(response)
 
 
-    def _get_woz_woningen_from(self, kadastraal_object):
-        woz_objecten = []
+    def _get_woz_woningen_from(self, kadastrale_identificatie):
         woz_kadastraal_objecten = models.WOZKadastraalObject.objects.filter(
-            kadastraal_object_identificatie=kadastraal_object
+            kadastrale_gemeentecode=kadastrale_identificatie[0],
+            sectie=kadastrale_identificatie[1],
+            perceelnummer=kadastrale_identificatie[2],
+            indexletter=kadastrale_identificatie[3],
+            indexnummer=kadastrale_identificatie[4],
         ).all()
 
+        woz_objecten = []
         for woz_kadastraal_object in woz_kadastraal_objecten:
             woz_object = models.WOZObject.objects.get(
                 woz_objectnummer=woz_kadastraal_object.woz_object_id
