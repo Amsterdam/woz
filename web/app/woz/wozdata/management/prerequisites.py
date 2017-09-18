b4ae7295137ecd3c24efe4feaa0bce27ede3d784
Marten Hoekstra <marten@dolhuis.nl>
2017-09-18 12:34:57 +0200

    repareert wachtscript voor de import

--- web/app/woz/wozdata/management/prerequisites.py
+++ web/app/woz/wozdata/management/prerequisites.py
@@ -6,22 +6,22 @@ from django.db import connection
 log = logging.getLogger(__name__)
 
 
-def is_table_present(source_table_name):
+def is_table_present():
     try:
         with connection.cursor() as cursor:
-            cursor.execute("select * from information_schema.tables where table_name=%s", (source_table_name,))
+            cursor.execute("select * from information_schema.tables where table_name='nummeraanduiding_gebruiksdoelen'")
             return bool(cursor.rowcount)
     except Exception as e:
         log.error(e)
         return False
 
 
-def fill_referentiedata(source_table_name):
+def fill_referentiedata():
     while True:
-        log.warning(f"waiting for {source_table_name} table...")
+        log.warning(f"waiting for nummeraanduiding_gebruiksdoelen table...")
         time.sleep(30)
-        if is_table_present(source_table_name) is True:
-            log.warning(f"done... waiting for {source_table_name} table")
+        if is_table_present() is True:
+            log.warning(f"done... waiting for nummeraanduiding_gebruiksdoelen table")
             break
 
     try:
@@ -29,8 +29,8 @@ def fill_referentiedata(source_table_name):
             cursor.execute("""
 INSERT INTO wozdata_nummeraanduidinggebruiksdoel (nummeraanduiding, code, omschrijving)
     SELECT id as nummeraanduiding, code, omschrijving
-    FROM %s
-            """, (source_table_name))
+    FROM nummeraanduiding_gebruiksdoelen
+            """)
             return bool(cursor.rowcount)
     except Exception as e:
         log.error(e)
