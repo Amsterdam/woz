42e98cae3782fecfc6f77b10802b56286f29ac5c
Marten Hoekstra <marten@dolhuis.nl>
2017-09-25 15:17:54 +0200

    tg-1222 codereview

--- web/app/woz/settings.py
+++ web/app/woz/settings.py
@@ -1,5 +1,4 @@
-import os
-from woz.settings_common import INSTALLED_APPS, DEBUG
+from woz.settings_common import *
 from woz.settings_databases import Location_key, \
     get_docker_host, \
     get_database_key, \
