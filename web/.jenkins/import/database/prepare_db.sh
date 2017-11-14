b24d0dc4a5417380055acea5c777e1034bbfa538
Marten Hoekstra <marten@dolhuis.nl>
2017-11-14 10:22:04 +0100

    Update db script

--- web/.jenkins/import/database/prepare_db.sh
+++ web/.jenkins/import/database/prepare_db.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-/bin/update-db.sh atlas
+/bin/update-db.sh atlas root
 psql -U atlas -c 'create table nummeraanduiding_gebruiksdoelen as select n.landelijk_id id, g.code, g.omschrijving from bag_nummeraanduiding n, bag_gebruiksdoel g where n.verblijfsobject_id = g.verblijfsobject_id;'
 pg_dump  -U atlas -t nummeraanduiding_gebruiksdoelen atlas | psql -U woz -d woz
 psql -U woz -d woz -c 'ALTER TABLE nummeraanduiding_gebruiksdoelen OWNER TO woz;'
